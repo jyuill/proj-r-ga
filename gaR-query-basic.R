5f5f5f0df0f1e4363874f7038cebc0857aaacb16
jyuill <john@fig4.com>
2016-07-09 18:13:43 -0700

    issues with v4 api

--- gaR-query-basic.R
+++ gaR-query-basic.R
@@ -30,12 +30,11 @@ ga_id <- account_list %>% filter(viewName=="01 SWBF") %>% select(viewId)
 ## filter for rows, find viewid or name and use one of the commands above
 ga_idsearch <- account_list %>% filter(grepl("FIFA",viewName))
 
-### simplest query
-gadata <- google_analytics(id = ga_id,
-                           start="2016-06-01", end="2016-06-02",
-                           metrics = c("sessions", "bounceRate"),
-                           dimensions = c("source", "medium"),
-                           max=150) ## default max=100
+### simplest query API v4
+ga_data <- google_analytics_4(ga_id,
+                              date_range = c("2016-06-01","2016-06-02"),
+                              dimensions=c('source','medium'),
+                              metrics = c('sessions','bounces'))
 
 ### query 2: creating variables outside query for ease of use/reuse
 start <- "2016-06-15"
@@ -79,7 +78,66 @@ gadata <- google_analytics(id = ga_id,
                            dimensions = dimensions,
                            max=150) ## default max=100
 
+### query 4: filters
+start <- "2016-06-15"
+end <- "2016-06-15"
+
+## metrics
+sess <- "sessions"
+pv <- "pageviews"
+upv <- "uniquePageviews"
+bounces <- "bounces"
+br <- "bounceRate"
+
+metrics <- c(sess)
+
+## create filters on metrics
+mf <- met_filter(sess, "GREATER_THAN", 10)
+#mf2 <- met_filter("sessions", "GREATER", 2)
+
+## dimensions
+dt <- "date"
+yr <- "year"
+mth <- "month"
+med <- "medium"
+src <- "source"
+pg <- "pagePath"
+
+dimensions <- c(dt,src)
+
+## create filters on dimensions
+df <- dim_filter(src,"BEGINS_WITH","e",not=FALSE)
+df2 <- dim_filter("source","BEGINS_WITH","ea",not = TRUE)
+
+## construct filter objects
+# fcd <- filter_clause_ga4(list(df, df2), operator = "AND")
+# fcm <- filter_clause_ga4(list(mf, mf2), operator = "AND")
+fcm <- filter_clause_ga4(list(mf))
+fcd <- filter_clause_ga4(list(df,df2),operator = "AND")
+
+gadata <- google_analytics(id = ga_id,
+                           start=start, end=end,
+                           metrics = metrics,
+                           dimensions = dimensions,
+                           #met_filters = fcm,
+                           dim_filters = fcd,
+                           max=150) ## default max=100
 
 
-
-
+## create filters on metrics
+mf <- met_filter("bounces", "GREATER_THAN", 0)
+mf2 <- met_filter("sessions", "GREATER", 2)
+## create filters on dimensions
+df <- dim_filter("source","BEGINS_WITH","1",not = TRUE)
+df2 <- dim_filter("source","BEGINS_WITH","a",not = TRUE)
+## construct filter objects
+fc2 <- filter_clause_ga4(list(df, df2), operator = "AND")
+fc <- filter_clause_ga4(list(mf, mf2), operator = "AND")
+## make v4 request
+ga_data1 <- google_analytics_4(ga_id,
+                               date_range = c("2016-06-20","2016-06-21"),
+                               dimensions=c('source','medium'),
+                               metrics = c('sessions','bounces'),
+                               met_filters = fc,
+                               dim_filters = fc2,
+                               filtersExpression = "ga:source!=(direct)")
