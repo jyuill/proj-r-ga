bf7f012ce39f3e394e75ffa3a8949e93d03f05a3
jyuill <john@fig4.com>
2016-07-10 19:30:47 -0700

    api v4 filter and order

--- gaR-query-basic.R
+++ gaR-query-basic.R
@@ -17,8 +17,7 @@ library(dplyr)
 ## options("googleAuthR.scopes.selected" = c("https://www.googleapis.com/auth/analytics",
 ##                                          "https://www.googleapis.com/auth/analytics.readonly"))
 
-googleAuthR::gar_auth() ## actually works fine without options - need to investigate... 
-## ...to ensure works with options
+googleAuthR::gar_auth() ## will work without above options, but better for API quotas 
 
 account_list <- google_analytics_account_list()
 
@@ -49,7 +48,7 @@ dimensions <- c("date","medium")
 ga_data <- google_analytics_4(ga_id,
                               date_range = c(start,end),
                               metrics=metrics,
-                              dimensions=dimension)
+                              dimensions=dimensions)
 
 ### query 3: creating lists of possible variables for reference
 start <- "2016-06-15"
@@ -87,13 +86,15 @@ end <- "2016-06-15"
 sess <- "sessions"
 pv <- "pageviews"
 upv <- "uniquePageviews"
+ent <- "entrances"
 bounces <- "bounces"
 br <- "bounceRate"
+exit <- "exits"
 
-metrics <- c(sess)
+metrics <- c(pv,upv,bounces)
 
 ## create filters on metrics
-mf <- met_filter(sess, "GREATER_THAN", 10)
+mf <- met_filter(pv, "GREATER_THAN", 10)
 #mf2 <- met_filter("sessions", "GREATER", 2)
 
 ## dimensions
@@ -124,20 +125,67 @@ ga_data <- google_analytics_4(ga_id,
                               dim_filters=fcd)
 
 
+####### EXAMPLE CODE: create filters on mutliple metrics and dimensions
+# mf <- met_filter("bounces", "GREATER_THAN", 0)
+# mf2 <- met_filter("sessions", "GREATER", 2)
+# ## create filters on dimensions
+# df <- dim_filter("source","BEGINS_WITH","1",not = TRUE)
+# df2 <- dim_filter("source","BEGINS_WITH","a",not = TRUE)
+# ## construct filter objects
+# fc2 <- filter_clause_ga4(list(df, df2), operator = "AND")
+# fc <- filter_clause_ga4(list(mf, mf2), operator = "AND")
+######## END OF EXAMPLE CODE
+
+### query 5: ordering results
+start <- "2016-06-15"
+end <- "2016-06-15"
+
+## metrics
+sess <- "sessions"
+pv <- "pageviews"
+upv <- "uniquePageviews"
+ent <- "entrances"
+bounces <- "bounces"
+br <- "bounceRate"
+exit <- "exits"
+
+metrics <- c(pv,upv,bounces)
+
 ## create filters on metrics
-mf <- met_filter("bounces", "GREATER_THAN", 0)
-mf2 <- met_filter("sessions", "GREATER", 2)
+mf <- met_filter(pv, "GREATER_THAN", 10)
+#mf2 <- met_filter("sessions", "GREATER", 2)
+
+## dimensions
+dt <- "date"
+yr <- "year"
+mth <- "month"
+med <- "medium"
+src <- "source"
+pg <- "pagePath"
+
+dimensions <- c(dt,pg)
+
 ## create filters on dimensions
-df <- dim_filter("source","BEGINS_WITH","1",not = TRUE)
-df2 <- dim_filter("source","BEGINS_WITH","a",not = TRUE)
+df <- dim_filter(pg,"REGEX","news",not=FALSE)
+#df2 <- dim_filter("source","BEGINS_WITH","ea",not = TRUE)
+
 ## construct filter objects
-fc2 <- filter_clause_ga4(list(df, df2), operator = "AND")
-fc <- filter_clause_ga4(list(mf, mf2), operator = "AND")
-## make v4 request
-ga_data1 <- google_analytics_4(ga_id,
-                               date_range = c("2016-06-20","2016-06-21"),
-                               dimensions=c('source','medium'),
-                               metrics = c('sessions','bounces'),
-                               met_filters = fc,
-                               dim_filters = fc2,
-                               filtersExpression = "ga:source!=(direct)")
+# fcd <- filter_clause_ga4(list(df, df2), operator = "AND")
+# fcm <- filter_clause_ga4(list(mf, mf2), operator = "AND")
+fcm <- filter_clause_ga4(list(mf))
+fcd <- filter_clause_ga4(list(df))
+
+## construct order type object in format of example:
+# order_type(field, sort_order = c("ASCENDING", "DESCENDING"),
+#            orderType = c("VALUE", "DELTA", "SMART", "HISTOGRAM_BUCKET",
+#                          "DIMENSION_AS_INTEGER"))
+
+ord <- order_type(pv, sort_order="DESCENDING", orderType="VALUE")
+
+ga_data <- google_analytics_4(ga_id,
+                              date_range = c(start,end),
+                              metrics=metrics,
+                              dimensions=dimensions,
+                              met_filters=fcm,
+                              dim_filters=fcd,
+                              order=ord)
